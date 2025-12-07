02b420a7d760de4afa9464d516a650c764ff4694
crisdevia101 <cristian.devia@udea.edu.co>
2025-12-07 13:32:55 -0500

    funcion principal

--- main.py
+++ main.py
@@ -1,6 +1,28 @@
-from controlador.main_controller import MainController
+from PyQt5.QtWidgets import QApplication
+import sys
+import resources_rc
+
+
+from modelo.modelo_1 import Model
+from controlador.controlador_login import ControladorLogin
+from vista.vista import LoginView
+
+
+def main():
+    app = QApplication(sys.argv)
+
+    model = Model()
+
+    login_view = LoginView()
+
+    controlador_login = ControladorLogin(login_view, model)
+
+    controlador_login.show()
+
+    sys.exit(app.exec_())
+
 
 if __name__ == "__main__":
-    app = MainController()
-    app.ejecutar()
+    main()
+
 
